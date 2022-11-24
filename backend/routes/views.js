9c900cba43a971bd6c48b5045dc2084d9db8a2ca
Saurabhpl2408 <saurabhpl14@gmail.com>
2022-11-24 11:43:52 +0530

    Added partogram details form

--- backend/routes/views.js
+++ backend/routes/views.js
@@ -3,14 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  if(req.isAuthenticated())
-    res.redirect('/doctorsDashboard');
+  if (req.isAuthenticated()) { res.redirect('/doctorsDashboard'); }
   res.render('index');
 });
 
 router.get('/login', (req, res) => {
-  if(req.isAuthenticated())
-    res.redirect('/doctorsDashboard');
+  if (req.isAuthenticated()) { res.redirect('/doctorsDashboard'); }
   res.render('login');
 });
 
@@ -22,8 +20,7 @@ router.get('/doctorsDashboard', (req, res) => {
 module.exports = router;
 
 router.get('/register', (req, res) => {
-  if(req.isAuthenticated())
-    res.redirect('/doctorsDashboard');
+  if (req.isAuthenticated()) { res.redirect('/doctorsDashboard'); }
   res.render('register');
 });
 
@@ -31,4 +28,12 @@ router.get('/addpatient', (req, res) => {
   res.render('addPatient', { data: {} });
 });
 
+// router.get('/addReadings', (req, res) => {
+//   res.render('addReadings', { data: {} });
+// });
+// module.exports = router;
+
+router.get('/measurements', (req, res) => {
+  res.render('measurements', { data: {} });
+});
 module.exports = router;
